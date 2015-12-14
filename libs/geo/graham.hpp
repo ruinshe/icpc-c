6ea9577d0f2d668e34686d3dca9e74f1139a227b
Ruins He <lyhypacm@gmail.com>
2015-12-14 16:54:31 +0800

    auto sync.

--- libs/geo/graham.hpp
+++ libs/geo/graham.hpp
@@ -10,24 +10,24 @@ double graham(point_t<T>* p, int n) {
 
   if (n < 3) return 0.0;
 
-  std::sort(p, p + n, compCoord<double>);
+  std::sort(p, p + n, compCoord<T>);
   stack[top++] = p[1];
   stack[top++] = p[2];
 
-  for (int i = 3; i <= n; i++) {
+  for (int i = 2; i < n; i++) {
     while (top >= 2 && xmult(p[i], stack[top - 1], stack[top - 2]) >= 0) top--;
     stack[top++] = p[i];
   }
 
   int tmp = top;
-  for (int i = n - 1; i >= 1; i--) {
+  for (int i = n - 2; i >= 0; i--) {
     while (top != tmp && xmult(p[i], stack[top - 1], stack[top - 2]) >= 0) top--;
     stack[top++] = p[i];
   }
   top--;
+  stack[top] = stack[0];
 
   for (int i = 0; i < top; i++) res += stack[i] * stack[i + 1];
-  printf("res = %.12f\n", fabs(res) / 2.0);
   return fabs(res) / 2.0;
 }
 
