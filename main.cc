5276fe569d9af1f752bef8ee4dbd31e0544f9832
Ruins He <lyhypacm@gmail.com>
2016-12-14 11:42:31 +0800

    Add more useful includes.

--- main.cc
+++ main.cc
@@ -1,10 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 #include <algorithm>
 #include <queue>
 #include <vector>
 #include <numeric>
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
 #include <functional>
 
 int main() {
