e41e08c1f17375888b00faff3f633bfdbcff528b
Ruins He <lyhypacm@gmail.com>
2016-12-30 16:36:19 +0800

    add DSU implementation.

---
+++ libs/ds/dsu.hpp
@@ -0,0 +1,20 @@
+#ifndef DSU_H
+#define DSU_H
+
+struct dsu_t {
+  int f[MaxN];
+
+  void init(int n = MaxN) {
+    for (int i = 0; i < n; i++) f[i] = i;
+  }
+
+  int getf(int x) {
+    return f[x] == x ? x : f[x] = getf(f[x]);
+  }
+
+  void merge_to_f(int x, int p) {
+    f[getf(x)] = p;
+  }
+};
+
+#endif // DSU_H
--- libs/headers.hpp
+++ libs/headers.hpp
@@ -12,6 +12,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <functional>
+#include <tuple>
 #include <cassert>
 
 #define for_each_test_cases(T) int T; scanf("%d", &T); for (int cas = 1; cas <= T; cas++)
